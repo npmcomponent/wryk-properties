7594322db13dc09e532f344209fc32bcbc4cc9dd
Mathieu Gallé-Tessonneau <mathieu.galletessonneau@gmail.com>
2013-08-13 02:48:34 +0200

    all source code changed, new api

--- index.js
+++ index.js
@@ -1,27 +1,84 @@
-'use strict';
-
 var defaults = require('defaults');
 
-/**
- * @param {Object} object
- * @param {Object} definitions
- * @param {Object} [defaultDescriptor = {}]
- * @param {String} [usedDescriptorProperty = 'value']
- *
- * @return {Object}
-**/
-module.exports = function (object, definitions, defaultDescriptor, usedDescriptorProperty) {
-	defaultDescriptor = defaultDescriptor || {};
-	usedDescriptorProperty = usedDescriptorProperty || 'value';
-
-	Object.keys(definitions).forEach(function (property) {
-		var descriptor = {};
-		descriptor[usedDescriptorProperty] = definitions[property];
-
-		Object.defineProperty(object, property, defaults(descriptor, defaultDescriptor));
-	});
-
-	return object;
+module.exports = function (object) {
+	return new RootWrapper(object);
 };
 
-//aaaa
+
+
+// RootWrapper
+function RootWrapper (object) {
+	this._object = object;
+	this._default = {};
+};
+
+RootWrapper.prototype.default = function (descriptor) {
+	this._default = descriptor;
+};
+
+RootWrapper.prototype.property = function (property) {
+	return new PropertyWrapper(this._object, property);
+};
+
+
+
+
+// PropertyWrapper
+function PropertyWrapper (object, property, parent) {
+	this._object = object;
+	this._default = {};
+	this._property = property;
+	this._parent = parent;
+	this._descriptor = {};
+}
+
+PropertyWrapper.prototype.default = RootWrapper.prototype.default;
+
+PropertyWrapper.prototype.property = function (property, descriptor) {
+	var child = new PropertyWrapper(this._object[this._property], property, this);
+	
+	if (descriptor) {
+		child._descriptor = descriptor;
+		return child.define();
+	} else {
+		return child;
+	}
+};
+
+PropertyWrapper.prototype.define = function () {
+	var descriptors = [this._descriptor];
+	var wrapper = this;
+
+	while (wrapper = wrapper._parent) {
+		descriptors.push(wrapper._descriptor);
+	}
+
+	Object.defineProperty(this._object, this._property, defaults.apply(null, descriptors));
+	return this._parent;
+};
+
+
+
+PropertyWrapper.prototype.configurable = function (value) {
+	this._descriptor.configurable = value === undefined ? true : value;
+};
+
+PropertyWrapper.prototype.enumerable = function (value) {
+	this._descriptor.enumerable = value === undefined ? true : value;
+};
+
+PropertyWrapper.prototype.value = function (value) {
+	this._descriptor.value = value === undefined ? this._object[this._property] : value;
+};
+
+PropertyWrapper.prototype.writable = function (value) {
+	this._descriptor.writable = value === undefined ? true : value;
+};
+
+PropertyWrapper.prototype.getter = function (value) {
+	this._descriptor.get = value;
+};
+
+PropertyWrapper.prototype.setter = function (value) {
+	this._descriptor.set = value;	
+};
